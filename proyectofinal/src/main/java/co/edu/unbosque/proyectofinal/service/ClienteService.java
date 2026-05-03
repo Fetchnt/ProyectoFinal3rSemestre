67a7b7c074e7cad43280ec0ad0e035904c6650e7
Fetchnt <delsinrowe752@gmail.com>
2026-05-03 16:13:37 -0500

    Patch 1.1

--- proyectofinal/src/main/java/co/edu/unbosque/proyectofinal/service/ClienteService.java
+++ proyectofinal/src/main/java/co/edu/unbosque/proyectofinal/service/ClienteService.java
@@ -1,8 +1,52 @@
 package co.edu.unbosque.proyectofinal.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import co.edu.unbosque.proyectofinal.entity.Cliente;
+import co.edu.unbosque.proyectofinal.repository.ClienteRepository;
+
 @Service
-public class ClienteService {
+public class ClienteService implements CRUDOPERATION<Cliente>{
+
+	@Autowired
+	private ClienteRepository cRep;
+	
+	
+	@Override
+	public int create(Cliente data) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int deleteById(Long id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public long count() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean exist(Long id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int updateById(Long id, Cliente data) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
--- proyectofinal/src/main/java/co/edu/unbosque/proyectofinal/service/CodigoService.java
+++ proyectofinal/src/main/java/co/edu/unbosque/proyectofinal/service/CodigoService.java
@@ -1,8 +1,51 @@
 package co.edu.unbosque.proyectofinal.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import co.edu.unbosque.proyectofinal.entity.Codigo;
+import co.edu.unbosque.proyectofinal.repository.CodigoRepository;
+
 @Service
-public class CodigoService {
+public class CodigoService implements CRUDOPERATION<Codigo>{
+
+	@Autowired
+	private CodigoRepository coRep;
+	
+	@Override
+	public int create(Codigo data) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int deleteById(Long id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public long count() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean exist(Long id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int updateById(Long id, Codigo data) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
