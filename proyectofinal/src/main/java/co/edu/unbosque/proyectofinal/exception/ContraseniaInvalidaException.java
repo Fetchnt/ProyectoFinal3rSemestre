e19be787eb7ff38e5b6a447c55af64c0f379e71b
jcabarcasr <jcabarcasr@unbosque.edu.co>
2026-05-19 22:04:56 -0500

    Excepciones hechas e integradas en usuarioservice

--- proyectofinal/src/main/java/co/edu/unbosque/proyectofinal/exception/ContraseniaInvalidaException.java
+++ proyectofinal/src/main/java/co/edu/unbosque/proyectofinal/exception/ContraseniaInvalidaException.java
@@ -1,9 +1,9 @@
 package co.edu.unbosque.proyectofinal.exception;
 
-public class ContraseniaInvalidaException extends RuntimeException {
+public class ContraseniaInvalidaException extends Exception {
 
-	public ContraseniaInvalidaException(String mensaje) {
-		super(mensaje);
+	public ContraseniaInvalidaException() {
+	    super("La contraseña no es válida. Debe tener al menos 12 caracteres y no estar vacía.");
 	}
 
 }
